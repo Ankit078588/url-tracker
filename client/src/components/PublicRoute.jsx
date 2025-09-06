40c8739cc264ac1e4c9f851929a1e10a45606f43
Ankit <ankit.dev.78588@gmail.com>
2025-09-06 23:20:20 +0530

    Added Loader in PublicRoute

--- client/src/components/PublicRoute.jsx
+++ client/src/components/PublicRoute.jsx
@@ -1,12 +1,13 @@
 import { useContext } from "react"
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
+import Loader from "./Loader";
 
 
 const PublicRoute = ({children}) => {
     const { isLoading, isAuthenticated } = useContext(AuthContext);
     
-    if(isLoading) return (<div className="h-screen"><p>Loading...</p></div>)
+    if(isLoading) return <Loader />
     if(isAuthenticated) return <Navigate to='/dashboard' />
 
     return children;
